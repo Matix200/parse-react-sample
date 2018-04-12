c655b8b258038982f5108b7241383e2e7ef91614
Matix200 <mateusz.mitek95@gmail.com>
2018-04-12 12:28:44 +0200

    Update devServer.js

--- devServer.js
+++ devServer.js
@@ -22,6 +22,3 @@ app.get('*', function(req, res) {
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
-
-  console.log('Listening at http://localhost:3000');
-});
